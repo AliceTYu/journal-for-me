b93cfb307e57ad8a5e796d2debbc8cc4ba103888
AliceTYu <turaeva.alice@gmail.com>
2024-08-06 13:50:27 +0300

    add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/journal-for-me/'
+   plugins: [react()],
+   base: '/journal-for-me/'
 });
