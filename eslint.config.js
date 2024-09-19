640869e7e37b53559d65b20ad8fae6be5afb29c5
AliceTYu <turaeva.alice@gmail.com>
2024-09-19 12:57:32 +0300

    fix2

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,7 @@ export default [
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh
 		},
+		base: '/journal-for-me',
 		rules: {
 			...js.configs.recommended.rules,
 			...react.configs.recommended.rules,
