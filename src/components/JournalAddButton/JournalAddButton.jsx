14f1d273ee656090dbf52101777add8c3962dd42
AliceTYu <turaeva.alice@gmail.com>
2024-09-19 13:29:06 +0300

    fix4

--- src/components/JournalAddButton/JournalAddButton.jsx
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -4,7 +4,7 @@ import styles from './JournalAddButton.module.css';
 function JournalAddButton({addNewItem}) {
 	return (
 		<CardButton className={styles.journalAdd} onClick={addNewItem}>
-			<img src="/plus.svg" alt="plus" />
+			<img src="./public/plus.svg" alt="plus" />
 				Новое воспоминание
 		</CardButton>
 	);
